0b0d71140aa68cc9c6d0dccc6f44e4f84c439d66
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-04-28 18:55:07 -0700

    coin giration and movement slime

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("WORKING")
 
-var sidelength = 30//(Math.floor(Math.random()*80)+50);
+var sidelength = 20//(Math.floor(Math.random()*80)+50);
 
 var tempWorld = [];
 var world = [];
@@ -36,6 +36,7 @@ var output = "";
             }
         }
     }
+    console.table(tempWorld)
     while(tempWorld.length > 0) {
         let temp;
         temp = tempWorld.splice(0,sidelength);
@@ -61,124 +62,108 @@ function drawWorld(){
 drawWorld();
 
 function updateMap(){
-    world[pacman.y][pacman.x] = 1;
+    world[Math.round(pacman.y)][Math.round(pacman.x)] = 1;
     drawWorld();
 }
 
-
 var pacman = {
-    x: Math.floor(sidelength/2),
-    y: Math.floor(sidelength/2),
+    x: sidelength/2,
+    y: sidelength/2,
 }
 
-console.log(pacman.x);
-console.log(pacman.y);
 
 var docPacman = document.getElementById("pacman");
 
-console.log(docPacman);
-
 function drawPacman() {
     docPacman.style.top = (pacman.y * 20) + "px";
-    docPacman.style.top = ((pacman.y * 20) + .25) + "px";
-    docPacman.style.top = ((pacman.y * 20) + .5) + "px";
-    docPacman.style.top = ((pacman.y * 20) + .75) + "px";
-
-    docPacman.style.left = (pacman.x *20) + "px";
-    docPacman.style.left = ((pacman.x *20) +.25) + "px";
-    docPacman.style.left = ((pacman.x * 20) + .5) + "px";
-    docPacman.style.left = ((pacman.x * 20) + .75) + "px";
+    docPacman.style.left = (pacman.x * 20) + "px";
 
 }
 
-
-
-    world[pacman.y][pacman.x] = 0;
     drawPacman();
 
 
     var moving;
     //movement pacman
 document.onkeydown = function(e) {
-    
     if(e.keyCode == 37) {
         //LEFT
-        stopMovin();
+        pacman.y = Math.round(pacman.y)
+        clearInterval(moving)
         docPacman.style.transform = "rotate(180deg)"
-        moving = setInterval(keepMoving,100,37);
-        console.log(e.keyCode)  
-    } if(e.keyCode == 39) {
+        moving = setInterval(function() {
+            if(pacman.x.toFixed(1) % 1 == 0) {
+                if(world[pacman.y][Math.round(pacman.x) - 1] == 0) {
+                    clearInterval(moving);
+                } else {
+                    pacman.x-=0.10
+                }
+            } else {
+                pacman.x-=0.10;
+            }
+        }, 10)
+    } 
+    if(e.keyCode == 39) {
         //RIGHT
-        stopMovin();
+        pacman.y = Math.round(pacman.y)
+        clearInterval(moving)
         docPacman.style.transform = "rotate(0deg)"
-        moving = setInterval(keepMoving,100,39);
-        console.log(e.keyCode)
-    } else if(e.keyCode == 38) {
-        //DOWN
-        stopMovin();
-        docPacman.style.transform = "rotate(270deg)"
-        moving = setInterval(keepMoving,100,38);
-        console.log(e.keyCode)
-    } else if(e.keyCode== 40) {
+        moving = setInterval(function() {
+            if(pacman.x.toFixed(1) % 1 == 0) {
+                if(world[pacman.y][Math.floor(pacman.x) + 1] == 0) {
+                    clearInterval(moving);
+                } else{
+                    pacman.x+=0.10
+                }
+            } else {
+                pacman.x+=0.10;
+            }
+        }, 10)
+    } 
+    if(e.keyCode == 38) {
         //UP
-        stopMovin();
+        pacman.x = Math.round(pacman.x)
+        clearInterval(moving)
+        docPacman.style.transform = "rotate(270deg)"
+        moving = setInterval(function() {
+            if(pacman.y.toFixed(1) % 1 == 0) {
+                if(world[Math.round(pacman.y) - 1][pacman.x] == 0) {
+                    clearInterval(moving);
+                } else {
+                    pacman.y-=.10
+                }
+            } else {
+                pacman.y-=0.10;
+            }
+        }, 10)
+    }
+    if(e.keyCode== 40) {
+        //DOWN
+        pacman.x = Math.round(pacman.x)
+        clearInterval(moving)
         docPacman.style.transform = "rotate(90deg)"
-        moving = setInterval(keepMoving,100,40);
-        console.log(e.keyCode)
+        moving = setInterval(function() {
+            if(pacman.y.toFixed(1) % 1 == 0) {
+                if(world[Math.floor(pacman.y) + 1][pacman.x] == 0) {
+                    clearInterval(moving);
+                    console.log("STOPPED")
+                } else {
+                    pacman.y+= 0.10;
+                }
+            } else {
+                pacman.y+=0.10;
+            }
+        }, 10)
     }
-}
-
-
-function stopMovin() {
-    clearInterval(moving);
-}
-
-//world [Y-VALUE] [X-VALUE]
-function keepMoving(direction) {
-    if(direction == 37) {
-        if(world[pacman.y][pacman.x-1] == 0) {
-            stopMovin();
-            console.log("stopped x: " + pacman.x);
-        } else {
-            pacman.x--;
-            console.log("x: " + pacman.x);
-        }
-    }  if(direction == 39) {
-        if(world[pacman.y][pacman.x+1] == 0) {
-            stopMovin();
-            console.log("stopped x: " + pacman.x);
-        } else {
-            pacman.x++;
-            console.log("x: " + pacman.x);
-        }
-    }  if(direction == 38) {
-        if(world[pacman.y-1][pacman.x] == 0) {
-            stopMovin();
-            console.log("stopped y: " + pacman.x);
-
-        } else {
-            pacman.y--;
-            console.log("y: " + pacman.y);
-        }
-    }  if(direction == 40) {
-        if(world[pacman.y+1][pacman.x] == 0) {
-            stopMovin();
-            console.log("stopped y: " + pacman.y);
-
-        } else {
-            pacman.y++;
-            console.log("y: " + pacman.y);
-        }
-    }
-    updateMap();
 }
 
 setInterval(game, 10)
 function game() {
     
     //drawWorld();
-    world[pacman.y][pacman.x] = 1;
+    updateMap();
+
     drawPacman();
-//  
+
 
 }
